761e5fce0c6df04af3f4c1879aa55aed9f58af41
Lingxiao Wang <lingxiaowang@lingxiaos-mbp.lan>
2020-06-05 23:47:10 +1000

    Added order numbers in the table

--- main.js
+++ main.js
@@ -1,96 +1,111 @@
-console.log("Connected")
+console.log("Connected");
 
-var fileInput = document.getElementById("file-selector")
-var output = {}
-var outputArr = []
+const fileInput = document.getElementById("file-selector");
+const output = {};
+const outputArr = [];
+const ordersArr = [];
 
-var dateTimeString = new Date().toLocaleString()
+const dateTimeString = new Date().toLocaleString();
 
-var tbodyDom = document.getElementById("table-body")
+const tbodyDom = document.getElementById("table-body");
 
-var readFile = function () {
-    var reader = new FileReader()
-    reader.onload = function () {
-        var lines = reader.result.split("\n")
-        var headerLine = lines[0].split(",")
-        console.log("Target columns: ", headerLine[16], headerLine[17])
+const readFile = function () {
+  const reader = new FileReader();
+  reader.onload = function () {
+    const lines = reader.result.split("\n");
+    const headerLine = lines[0].split(",");
+    console.log(
+      "Target columns: ",
+      headerLine,
+      headerLine[1],
+      headerLine[16],
+      headerLine[17]
+    );
 
-        lines.forEach((line, index) => {
-            if (index > 0) {
-                var lineArr = line.split(",")
-                var productName = lineArr[17]
-                var quantity = Number(lineArr[16])
+    lines.forEach((line, index) => {
+      if (index > 0) {
+        let lineArr = line.split(",");
+        let productName = lineArr[17];
+        let quantity = Number(lineArr[16]);
+        let orderNumber = lineArr[0];
 
-                if (!productName) {
-                    return
-                }
+        if (!productName) {
+          return;
+        }
 
-                if (output[productName]) {
-                    output[productName] = output[productName] + quantity
-                } else {
-                    output[productName] = quantity
-                }
-            }
-        })
+        if (output[productName]) {
+          output[productName].quantity += quantity;
+          output[productName].orders += `     ${orderNumber}`;
+        } else {
+          output[productName] = { quantity: 0, orders: "" };
+          output[productName].quantity = quantity;
+          output[productName].orders = orderNumber;
+        }
+      }
+    });
 
-        var keys = Object.keys(output)
-        var sortedKeys = keys.sort((a, b) => {
-            return (a > b) - 0.5
-        })
+    // sort output object by key name alphabetically
+    const keys = Object.keys(output);
+    const sortedKeys = keys.sort((a, b) => {
+      return (a > b) - 0.5;
+    });
 
-        sortedKeys.forEach((key) => {
-            var tempArr = [key, output[key]]
-            outputArr.push(tempArr)
-        })
+    // flat the structure, conver output object to array for futhur csv export
+    sortedKeys.forEach((key) => {
+      let tempArr = [key, output[key].quantity, output[key].orders];
+      outputArr.push(tempArr);
+    });
 
-        outputArr.forEach((el) => {
-            var tr = document.createElement("tr")
-            var td_name = document.createElement("td")
-            var name = el[0]
-            td_name.innerHTML = name
-            var td_qty = document.createElement("td")
-            td_qty.innerHTML = el[1]
-            tr.appendChild(td_name)
-            tr.appendChild(td_qty)
-            tbodyDom.appendChild(tr)
-        })
+    outputArr.forEach((el) => {
+      let tr = document.createElement("tr");
+      let td_name = document.createElement("td");
+      let name = el[0];
+      td_name.innerHTML = name;
+      let td_qty = document.createElement("td");
+      td_qty.innerHTML = el[1];
+      let td_orders = document.createElement("td");
+      td_orders.innerHTML = el[2];
+      tr.appendChild(td_name);
+      tr.appendChild(td_qty);
+      tr.appendChild(td_orders);
+      tbodyDom.appendChild(tr);
+    });
 
-        exportToCsv(outputArr)
-    }
-    // start reading the file. When it is done, calls the onload event defined above.
-    reader.readAsText(fileInput.files[0])
-}
+    exportToCsv(outputArr);
+  };
+  // start reading the file. When it is done, calls the onload event defined above.
+  reader.readAsText(fileInput.files[0]);
+};
 
-var exportToCsv = function (results) {
-    var csvString = ""
+const exportToCsv = function (results) {
+  let csvString = "";
 
-    results.unshift(["Product Name", "Qty"])
+  results.unshift(["Product Name", "Qty", "Orders"]);
 
-    results.forEach(function (rowItem, rowIndex) {
-        rowItem.forEach(function (colItem, colIndex) {
-            csvString += colItem + ","
-        })
-        csvString += "\r\n"
-    })
-    csvString =
-        "data:text/csv;charset=utf-8,\uFEFF" + encodeURIComponent(csvString)
-    var x = document.createElement("a")
-    x.setAttribute("href", csvString)
-    x.innerHTML = "Download"
-    x.setAttribute("download", `Newnorth Sales Data ${dateTimeString}.csv`)
-    x.classList.add("button")
-    document.getElementById("d-btn-area").appendChild(x)
-}
+  results.forEach(function (rowItem, rowIndex) {
+    rowItem.forEach(function (colItem, colIndex) {
+      csvString += colItem + ",";
+    });
+    csvString += "\r\n";
+  });
+  csvString =
+    "data:text/csv;charset=utf-8,\uFEFF" + encodeURIComponent(csvString);
+  const x = document.getElementById("download-btn");
+  x.setAttribute("href", csvString);
+  x.setAttribute("download", `Newnorth Sales Data ${dateTimeString}.csv`);
+  x.classList.add("is-primary");
+  x.removeAttribute("disabled");
+};
 
-fileInput.addEventListener("change", readFile)
+fileInput.addEventListener("change", readFile);
 
 function printData() {
-    var divToPrint = document.getElementById("output-table")
-    newWin = window.open("")
-    newWin.document.write(divToPrint.outerHTML)
-    newWin.print()
-    newWin.close()
+  const divToPrint = document.getElementById("output-table");
+  newWin = window.open("");
+  newWin.document.write(divToPrint.outerHTML);
+  newWin.print();
+  newWin.close();
 }
 
-const printBtn = document.getElementById("print-btn")
+const printBtn = document.getElementById("print-btn");
 // printBtn.addEventListener("click", printData)
